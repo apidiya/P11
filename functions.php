c5647550c867f476eda1ae633e0d5010b34c4dc9
Apidiya <128754190+apidiya@users.noreply.github.com>
2023-12-19 16:27:24 +0100

    ok

--- functions.php
+++ functions.php
@@ -1,9 +1,16 @@
 <?php
+
 // Ajout des styles personnalisés
 function enqueue_custom_styles() {
     wp_enqueue_style('custom-style', get_template_directory_uri() . '/sass/style.css');
 }
 add_action('wp_enqueue_scripts', 'enqueue_custom_styles');
+
+// Ajout du support pour la balise de titre
+function theme_slug_setup() {
+    add_theme_support('title-tag');
+}
+add_action('after_setup_theme', 'theme_slug_setup');
 
 // Enregistrement des menus
 function register_menus() {
@@ -15,12 +22,6 @@ function register_menus() {
     );
 }
 add_action('init', 'register_menus');
-
-// Ajout du support pour la balise de titre
-function theme_slug_setup() {
-    add_theme_support('title-tag');
-}
-add_action('after_setup_theme', 'theme_slug_setup');
 
 // Ajout de jQuery d'un CDN et des scripts JS personnalisés
 function script_JS_Custom() {
@@ -34,16 +35,13 @@ add_action('wp_enqueue_scripts', 'script_JS_Custom');
 
 // Désactivation des paragraphes automatiques dans Contact Form 7
 add_filter('wpcf7_autop_or_not', '__return_false');
-?>
 
-<?php 
-/**
- * Proper ob_end_flush() for all levels
- *
+/** Proper ob_end_flush() for all levels
  * This replaces the WordPress `wp_ob_end_flush_all()` function
- * with a replacement that doesn't cause PHP notices.
- */
+ * with a replacement that doesn't cause PHP notices. */
  remove_action( 'shutdown', 'wp_ob_end_flush_all', 1 );
 add_action( 'shutdown', function() {
     while ( @ob_end_flush() );
 } );
+
+?>
